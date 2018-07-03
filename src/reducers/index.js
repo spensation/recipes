b2d533ed84ff18897399552385cb1fdbce23a570
spensation <spensation@gmail.com>
2018-07-03 14:26:12 -0700

    created new branch - form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { recipeReducer } from './recipes_reducer';
 import { commentsReducer } from './comments_reducer';
+import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
   recipes: recipeReducer,
   recipe: recipeReducer,
   comments: commentsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
